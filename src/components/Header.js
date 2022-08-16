63ac7939dad5c4646e1ed128762c452d72ffc10e
Jhacoby K. Delgado <jkeithd28@gmail.com>
2022-08-15 20:58:28 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Header.css"
+
+export const Header = () => {
+    return (
+        <header>
+            <i className="fa-solid fa-earth-americas"></i>
+            <h3> my travel journal </h3>
+        </header>
+    )
+};
---
+++ src/components/Place.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Place.css";
+
+
+export const Place = ({id,title,location,maps,startDate,endDate,description,img}) => {
+    return(
+        <article>
+            <img src={img} />
+            <div className="text-content">
+                <div className="first-line"> <i className="fa-solid fa-location-dot"></i> <span className="country"> {location} </span> <a  href={maps} > View on Google Maps </a> </div>
+                <h1> {title} </h1>
+                <p className="date"> <strong> {`${startDate} - ${endDate}`} </strong> </p>
+                <p className="description"> {description} </p>
+            </div>
+        </article>
+    )
+}
